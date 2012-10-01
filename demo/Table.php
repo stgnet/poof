6fabcd644a78b4443a3d65fe6587a746b010dcec
Scott Griepentrog <scott@griepentrog.com>
2012-10-01 11:32:39 -0400

    trying travis-ci

--- demo/Table.php
+++ demo/Table.php
@@ -9,6 +9,8 @@
 		),
 		uiContainer()->Add(
 			uiHeader(3,"The PHP code that generated this page:"),
-			uiCodeMirror(file_get_contents($_SERVER['SCRIPT_FILENAME']))->AddClass("pre-scrollable")
+			uiCodeMirror(file_get_contents($_SERVER['SCRIPT_FILENAME']))->AddClass("pre-scrollable"),
+			uiHeader(3,"Contents of demodata.csv:"),
+			uiCodeMirror(file_get_contents("demodata.csv"))
 		)
 	);
